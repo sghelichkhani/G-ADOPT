f343a226c41af327d056f6f67031492e52948b08
w3168 <wis15@ic.ac.uk>
2023-07-18 16:08:10 +0100

    first go at elastic deformation
    following kramer et al 2012 free surface paper.
    
    Incompressible formulation with surface loading just intepret
    velocity as displacement and use shear modulus instead of viscosity

---
+++ demos/free_surface/elastic_surface.py
@@ -0,0 +1,89 @@
+from gadopt import *
+from mpi4py import MPI
+
+# Set up geometry:
+nx, ny = 40, 40
+L = 3e6 # length of domain in m
+D = L # Depth of the domain in m
+mesh = RectangleMesh(nx, ny, L, D)  # Rectangle mesh generated via firedrake
+left_id, right_id, bottom_id, top_id = 1, 2, 3, 4  # Boundary IDs
+
+# Set up function spaces - currently using the bilinear Q2Q1 element pair:
+V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+Z = MixedFunctionSpace([V, W])  # Mixed function space.
+
+# Function to store the solutions:
+z = Function(Z)  # a field over the mixed function space Z.
+u, p = split(z)  # Returns symbolic UFL expression for u and p
+
+T = Function(Q, name="Temperature").assign(0)
+# Output function space information:
+log("Number of Velocity DOF:", V.dim())
+log("Number of Pressure DOF:", W.dim())
+log("Number of Velocity and Pressure DOF:", V.dim()+W.dim())
+log("Number of Temperature DOF:", Q.dim())
+
+
+# Stokes related constants (note that since these are included in UFL, they are wrapped inside Constant):
+Ra = Constant(0)  # Rayleigh number
+approximation = BoussinesqApproximation(Ra)
+
+mu = 1e10 # Shear modulus in Pa
+
+steady_state_tolerance = 1e-9
+
+# Nullspaces and near-nullspaces:
+Z_nullspace = create_stokes_nullspace(Z, closed=True, rotational=False)
+
+# Write output files in VTK format:
+u, p = z.split() #subfunctions  # Do this first to extract individual velocity and pressure fields.
+# Next rename for output:
+u.rename("Displacement")
+p.rename("Pressure")
+# Create output file and select output_frequency:
+output_file = File("output_elastic_D_mu1e10.pvd")
+
+rho0 = 4500 # density in kg/m^3
+g = 10 # gravitational acceleration in m/s^2
+
+lam = D # wavelength of load in m
+k = 2 * pi / lam # wavenumber in m^-1
+F0 = 1000 # initial free surface amplitude in m
+X = SpatialCoordinate(mesh)
+eta = F0 * cos(k * X[0])
+n = FacetNormal(mesh)
+
+stokes_bcs = {
+    bottom_id: {'un': 0},
+    top_id: {'stress': -rho0 * g * eta * n},
+    left_id: {'un': 0},
+    right_id: {'un': 0},
+}
+
+stokes_solver = StokesSolver(z, T, approximation, bcs=stokes_bcs, mu=mu,
+                             cartesian=True,
+                             nullspace=Z_nullspace, transpose_nullspace=Z_nullspace)
+
+
+
+output_file.write(u, p)
+
+
+# Solve Stokes sytem:
+stokes_solver.solve()
+
+
+output_file.write(u, p)
+# Compute diagnostics:
+bcu = DirichletBC(u.function_space(), 0, top_id)
+ux_max = u.dat.data_ro_with_halos[bcu.nodes, 0].max(initial=0)
+ux_max = u.comm.allreduce(ux_max, MPI.MAX)  # Maximum Vx at surface
+
+
+
+
+with CheckpointFile("Final_State.h5", "w") as final_checkpoint:
+    final_checkpoint.save_mesh(mesh)
+    final_checkpoint.save_function(z, name="Stokes")
